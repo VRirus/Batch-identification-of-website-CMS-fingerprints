763be6951099104529f2fa6271ac263891bda8dc
TeamsSix <49087564+teamssix@users.noreply.github.com>
2019-12-17 17:59:09 +0800

    Update Batch_CMS_identification.py

--- Batch_CMS_identification.py
+++ Batch_CMS_identification.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 #by www.teamssix.com
 import sys
-import sys
 import zlib
 import json
 import requests
@@ -20,7 +19,7 @@ def results(url):
     result = {}
     request = whatweb(url)
     num = request.headers["X-RateLimit-Remaining"]
-    print(u"今日识别剩余次数",num)
+    print(u"\n\033[1;33;40m[!] 今日识别剩余次数 {}\033[0m".format(num))
     req_json = request.json()
     for i in req_json:
         sub_i = req_json[i][0]
@@ -31,18 +30,27 @@ def results(url):
 if __name__ == '__main__':
     pools = []
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'}
-    readDir = sys.argv[1]
-    file_name = sys.argv[2]
-    print('示例：python3 Batch_CMS_identification.py url.txt filename')
-    f = open(readDir,"r")
+    try:
+        readDir = sys.argv[1]
+        file_name = sys.argv[2]
+        f = open(readDir,"r")
+    except:
+        print('''\033[1;31;40m
+请检查你的参数是否有误
+
+示例：python3 Batch_CMS_identification.py url.txt filename
+        \033[0m''')
+        sys.exit()
+    print('\n\033[1;33;40m示例：python3 Batch_CMS_identification.py url.txt filename\033[0m')
+    
     for url in f.read().split():
         try:
             pools.append(results(url))
         except (requests.exceptions.ConnectionError,requests.exceptions.ReadTimeout):
-            print('连接异常，正在识别下一个URL……')
+            print('\033[1;31;40m[-] 连接异常，正在识别下一个URL……\033[0m')
             pass
         except BaseException as e:
-            print('程序发生'+str(e)+'异常','正在保存退出……')
+            print('\033[1;31;40m[-] 程序发生'+str(e)+'异常','正在保存退出……\033[0m')
             sys.exit()
         finally:
             df = pd.DataFrame(pools)

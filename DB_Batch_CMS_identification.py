ae2e92513b62fdf2febeab9e7de6072606ab443e
TeamsSix <49087564+teamssix@users.noreply.github.com>
2019-12-18 23:21:11 +0800

    Update DB_Batch_CMS_identification.py

--- DB_Batch_CMS_identification.py
+++ DB_Batch_CMS_identification.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 # by www.teamssix.com
+
+import io
 import sys
 import zlib
 import json
@@ -9,33 +11,28 @@ import pandas as pd
 
 
 def whatweb(url):
-	try:
-		response = requests.get(url, headers=headers, verify=False, allow_redirects=False,timeout=1)  # 如果本地网络环境延时较高，timeout可设置高一些，默认为1s
-		whatweb_dict = {"url": response.url, "text": response.text, "headers": dict(response.headers)}
-		whatweb_dict = json.dumps(whatweb_dict)
-		whatweb_dict = whatweb_dict.encode()
-		whatweb_dict = zlib.compress(whatweb_dict)
-		data = {"info": whatweb_dict}
-		return requests.post("http://whatweb.bugscaner.com/api.go", headers=headers, allow_redirects=False, files=data,timeout=1)  # 如果本地网络环境延时较高，timeout可设置高一些，默认为1s
-	except:
-		pass
+	response = requests.get(url, headers=headers, verify=False, allow_redirects=False,timeout=1)  # 如果本地网络环境延时较高，timeout可设置高一些，默认为1s
+	whatweb_dict = {"url": response.url, "text": response.text, "headers": dict(response.headers)}
+	whatweb_dict = json.dumps(whatweb_dict)
+	whatweb_dict = whatweb_dict.encode()
+	whatweb_dict = zlib.compress(whatweb_dict)
+	data = {"info": whatweb_dict}
+	return requests.post("http://whatweb.bugscaner.com/api.go", headers=headers, allow_redirects=False, files=data,timeout=1)  # 如果本地网络环境延时较高，timeout可设置高一些，默认为1s
 
 
 def results(url, title):
-	try:
-		result = {}
-		request = whatweb(url)
-		num = request.headers["X-RateLimit-Remaining"]
-		print(u"\n\033[1;33;40m[!] 今日识别剩余次数 {},正在识别 {}\033[0m".format(num, url))
-		req_json = request.json()
-		for i in req_json:
-			sub_i = req_json[i][0]
-			result[i] = sub_i
-		result['URL'] = url
-		result['title'] = title
-		return result
-	except:
-		pass
+	result = {}
+	request = whatweb(url)
+	num = request.headers["X-RateLimit-Remaining"]
+	print(u"\033[1;33;40m[!] {} 今日识别剩余次数 {},正在识别 {}\033[0m".format(numbers,num, url))
+	req_json = request.json()
+	for i in req_json:
+		i = i.replace('\xb7',' ')
+		sub_i = req_json[i][0]
+		result[i] = sub_i
+	result['URL'] = url.replace('\xb7',' ')
+	result['title'] = title.replace('\xb7',' ')
+	return result
 
 
 if __name__ == '__main__':
@@ -61,7 +58,7 @@ eg:   python3 DB_Batch_CMS_identification.py _ctrip_com
         \033[1;31;40m''')
 		sys.exit()
 
-	print('''\n\033[1;33;40m
+	print('''\033[1;33;40m
 	示例：python3 DB_Batch_CMS_identification.py db_name
 	eg:   python3 DB_Batch_CMS_identification.py _ctrip_com
 	        \033[0m''')
@@ -69,14 +66,17 @@ eg:   python3 DB_Batch_CMS_identification.py _ctrip_com
 		for numbers in range(len(url_list)):
 			try:
 				pools.append(results(url_list[numbers], title_list[numbers]))
-			except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
-				print('\033[1;31;40m[-] 连接异常，正在识别下一个URL……\033[0m')
-				pass
-			except BaseException as e:
-				print('\033[1;31;40m[-] 程序发生' + str(e) + '异常', '正在保存退出……\033[0m')
-				sys.exit()
-			finally:
 				df = pd.DataFrame(pools)
 				df.to_csv(r'{}.csv'.format(file_name), encoding='GB2312')
+			except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+				df = pd.DataFrame(pools)
+				df.to_csv(r'{}.csv'.format(file_name), encoding='GB2312')
+				print('\033[1;31;40m[-] {} 连接异常，正在识别下一个URL……\033[0m'.format(numbers))
+				pass
+			except BaseException as e:
+				df = pd.DataFrame(pools)
+				df.to_csv(r'{}.csv'.format(file_name), encoding='GB2312')
+				print('\033[1;31;40m[-] {} 程序发生' + str(e) + '异常', '正在保存退出……\033[0m'.format(numbers))
+				sys.exit()
 	else:
-		print('未读取到数据库文件，正在退出……')
+		print('\033[1;31;40m[-] 未读取到数据库文件，正在退出……\033[0m')

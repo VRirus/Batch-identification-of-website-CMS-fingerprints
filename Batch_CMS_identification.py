2c0e8b6ed39912d27705266716f28a040f1eacef
TeamsSix <49087564+teamssix@users.noreply.github.com>
2019-12-27 09:47:35 +0800

    Update Batch_CMS_identification.py

--- Batch_CMS_identification.py
+++ Batch_CMS_identification.py
@@ -8,20 +8,20 @@ import pandas as pd
 
 
 def whatweb(url):
-	response = requests.get(url='http://{}'.format(url), headers=headers, verify=False, allow_redirects=False)
+	response = requests.get(url='{}'.format(url), headers=headers, verify=False, allow_redirects=False,timeout=5)
 	whatweb_dict = {"url": response.url, "text": response.text, "headers": dict(response.headers)}
 	whatweb_dict = json.dumps(whatweb_dict)
 	whatweb_dict = whatweb_dict.encode()
 	whatweb_dict = zlib.compress(whatweb_dict)
 	data = {"info": whatweb_dict}
-	return requests.post("http://whatweb.bugscaner.com/api.go", headers=headers, allow_redirects=False, files=data)
+	return requests.post("http://whatweb.bugscaner.com/api.go", headers=headers, allow_redirects=False, files=data,timeout=5)
 
 
 def results(url):
 	result = {}
 	request = whatweb(url)
 	num = request.headers["X-RateLimit-Remaining"]
-	print(u"\n\033[1;33;40m[!] 今日识别剩余次数 {}\033[0m".format(num))
+	print(u"\n\033[1;33;40m[!] 今日识别剩余次数 {},正在识别{}\033[0m".format(num,url))
 	req_json = request.json()
 	for i in req_json:
 		sub_i = req_json[i][0]
@@ -41,23 +41,23 @@ if __name__ == '__main__':
 		print('''\033[1;31;40m
 请检查你的参数是否有误
 
-示例：python3 Batch_CMS_identification.py url.txt filename
+示例：python3 Batch_CMS_identification.py url.txt filename.csv
         \033[0m''')
 		sys.exit()
-	print('\n\033[1;33;40m示例：python3 Batch_CMS_identification.py url.txt filename\033[0m')
-
-	if f.read().split() != []:
-		for url in f.read().split():
+	print('\n\033[1;33;40m示例：python3 Batch_CMS_identification.py url.txt filename.csv\033[0m')
+	f = f.read().split('\n')
+	if f != []:
+		for url in f:
 			try:
 				pools.append(results(url))
 			except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
-				print('\033[1;31;40m[-] 连接异常，正在识别下一个URL……\033[0m')
+				print('\033[1;31;40m[-] 连接异常，正在识别下一个URL……\n\033[0m')
 				pass
 			except BaseException as e:
-				print('\033[1;31;40m[-] 程序发生' + str(e) + '异常', '正在保存退出……\033[0m')
-				sys.exit()
+				print('\033[1;31;40m[-] 程序发生' + str(e) + '异常，正在识别下一个URL……\n','\033[0m')
+				pass
 			finally:
 				df = pd.DataFrame(pools)
-				df.to_csv(r'{}.csv'.format(file_name), encoding='GB2312')
+				df.to_csv(r'{}'.format(file_name), encoding='GB2312')
 	else:
 		print('未读取到url文件，正在退出……')
